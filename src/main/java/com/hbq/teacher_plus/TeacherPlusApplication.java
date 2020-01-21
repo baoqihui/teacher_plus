5f16e965aa4a57f84bf85064e69af643fc9c5a38
huibaoqi <1318168098@qq.com>
2020-01-21 16:03:37 +0800

    Initial commit

---
+++ src/main/java/com/hbq/teacher_plus/TeacherPlusApplication.java
@@ -0,0 +1,13 @@
+package com.hbq.teacher_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hbq/teacher_plus/TeacherPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbq.teacher_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
