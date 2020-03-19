29f239655bcb8e01ab860bf13aeb40db2e647edf
huibaoqi <1318168098@qq.com>
2020-03-19 10:33:54 +0800

    全部基本信息导出完成。 项目功能基本完成！

--- src/main/java/com/hbq/teacher_plus/controller/BasicInfoController.java
+++ src/main/java/com/hbq/teacher_plus/controller/BasicInfoController.java
@@ -1,5 +1,6 @@
 package com.hbq.teacher_plus.controller;
 
+import cn.hutool.core.date.DateUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hbq.teacher_plus.common.model.PageResult;
 import com.hbq.teacher_plus.common.model.Result;
@@ -30,7 +31,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -148,15 +148,14 @@ public class BasicInfoController {
         ExcelUtil.exportExcel(basicInfos, "基本信息导出", "基本信息导出", BasicInfo.class, "basicInfo.xls", response);
 
     }
-    @ApiOperation(value = "基本信息导出")
+    @ApiOperation(value = "全部基本信息导出")
     @PostMapping("/basicInfo/leadOutAll")
     public void leadOutAll(String cuId, HttpServletResponse response) throws Exception {
 
-        Map<String, Object> params=new HashMap<>();
         String[] titles={"姓名","证件类型","证件号码","性别","出生年月",
                 "专业技术职务","最终学位",
-                "学位","所学专业","取得学位时间","毕业学校",
-                "学位","所学专业","取得学位时间","毕业学校",
+                "本科学位","所学专业","取得学位时间","毕业学校",
+                "硕士学位","所学专业","取得学位时间","毕业学校",
                 "参加工作时间","任教时间","到本专业任教时间",
                 "是否有行业经历","中青年教师实践教学能力培训","研究生导师"
         };
@@ -165,41 +164,52 @@ public class BasicInfoController {
 
         XSSFSheet sheet=(XSSFSheet) wk.getSheet("全部基本信息导出");
         List<BasicInfo> data = basicInfoService.list();
-        List<BasicInfoVo> basicInfoVos=new ArrayList<>();
-        List<Map<String,Object>> mapArrayList=new ArrayList<>();
-        XSSFRow row = sheet.createRow(data.size()+1);
+
+        int i=0;
         for (BasicInfo basicInfo : data) {
-            Map<String,Object> map=new HashMap<>();
-            System.out.println(basicInfo);
+            XSSFRow row = sheet.createRow(++i);
             cuId=basicInfo.getCuId();
             Work work = workService.getOne(new QueryWrapper<Work>().eq("cu_id", cuId));
-            List<Education> educations = educationService.list(new QueryWrapper<Education>().eq("cu_id", cuId));
-            map.put("basicInfo",basicInfo);
-            map.put("work",work);
-            map.put("educations",educations);
-            mapArrayList.add(map);
-
-
+            List<Education> educations = educationService.list(new QueryWrapper<Education>().eq("cu_id", cuId).orderByAsc("cert_id"));
 
             row.createCell(0).setCellValue(basicInfo.getName());
             row.createCell(1).setCellValue(basicInfo.getCertType());
             row.createCell(2).setCellValue(basicInfo.getCertNum());
             row.createCell(3).setCellValue(basicInfo.getSex());
 
-           /* row.createCell(4).setCellValue(doctors.get(i).getD_tel());
-            row.createCell(5).setCellValue(doctors.get(i).getD_plane());
-            row.createCell(6).setCellValue(doctors.get(i).getD_sex());
-            row.createCell(7).setCellValue(doctors.get(i).getD_age());
-            row.createCell(8).setCellValue(doctors.get(i).getD_date());
+            row.createCell(4).setCellValue(basicInfo.getBirthday());
+            row.createCell(5).setCellValue(basicInfo.getSkillJob());
 
-            row.createCell(9).setCellValue(doctors.get(i).getD_mail());
-            row.createCell(10).setCellValue(doctors.get(i).getDepartment().getDe_name());
-            row.createCell(11).setCellValue(doctors.get(i).getD_educ());
-            row.createCell(12).setCellValue(doctors.get(i).getD_remark());*/
+            if (educations.size()==1){
+                row.createCell(6).setCellValue(educations.get(0).getDegree());
+                row.createCell(7).setCellValue(educations.get(0).getDegree());
+                row.createCell(8).setCellValue(educations.get(0).getMajor());
+                row.createCell(9).setCellValue(DateUtil.format(educations.get(0).getGraduaTime(),"yyyy-MM-dd"));
+                row.createCell(10).setCellValue(educations.get(0).getSchool());
+            }
+            if (educations.size()==2){
+                row.createCell(6).setCellValue(educations.get(1).getDegree());
+                row.createCell(7).setCellValue(educations.get(0).getDegree());
+                row.createCell(8).setCellValue(educations.get(0).getMajor());
+                row.createCell(9).setCellValue(DateUtil.format(educations.get(0).getGraduaTime(),"yyyy-MM-dd"));
+                row.createCell(10).setCellValue(educations.get(0).getSchool());
+                row.createCell(11).setCellValue(educations.get(1).getDegree());
+                row.createCell(12).setCellValue(educations.get(1).getDegree());
+                row.createCell(13).setCellValue(DateUtil.format(educations.get(1).getGraduaTime(),"yyyy-MM-dd"));
+                row.createCell(14).setCellValue(educations.get(1).getSchool());
+            }
+            if (work!=null){
+                row.createCell(15).setCellValue(DateUtil.format(work.getWorkTime(),"yyyy-MM-dd"));
+                row.createCell(16).setCellValue(DateUtil.format(work.getTeachTime(),"yyyy-MM-dd"));
+                row.createCell(17).setCellValue(DateUtil.format(work.getAtTime(),"yyyy-MM-dd"));
+                row.createCell(18).setCellValue(work.getIsWork());
+                row.createCell(19).setCellValue(work.getTrain());
+                row.createCell(20).setCellValue(work.getSupervisor());
+            }
 
         }
 
-        OutputStream out= BuildPath.Manual_Saving(response, "Doctor.xls");
+        OutputStream out= BuildPath.Manual_Saving(response, "AllBasicInfo.xls");
         wk.write(out);
         wk.close();
 
