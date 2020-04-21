8b01147517c798771aca5677fb117e099849b2d7
hui <1318168098@qq.com>
2020-04-21 11:57:20 +0800

    sql日志分析更换

--- src/main/java/com/hbq/teacher_plus/config/DefaultMybatisPlusConfig.java
+++ src/main/java/com/hbq/teacher_plus/config/DefaultMybatisPlusConfig.java
@@ -1,10 +1,13 @@
 package com.hbq.teacher_plus.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Properties;
 
 /**
  * mybatis-plus配置
@@ -29,10 +32,15 @@ public class DefaultMybatisPlusConfig {
      * 打印 sql，性能分析拦截器，不建议生产使用
      * 设置 dev test 环境开启
      */
-
     /*@Bean
     public PerformanceInterceptor performanceInterceptor() {
-        return new PerformanceInterceptor();
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+        //格式化sql语句format改为true
+        Properties properties = new Properties();
+        properties.setProperty("format", "false");
+        performanceInterceptor.setProperties(properties);
+        return performanceInterceptor;
     }*/
+
 }
 
