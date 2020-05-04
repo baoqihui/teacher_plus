632e7ea3e99ec9242c362043cf04bc381d1e6099
hui <1318168098@qq.com>
2020-05-04 19:55:33 +0800

    人脸识别

--- src/main/java/com/hbq/teacher_plus/config/AlipayConfig.java
+++ src/main/java/com/hbq/teacher_plus/config/AlipayConfig.java
@@ -8,9 +8,9 @@ public class AlipayConfig {
     public static String APPID = "2016101500690876";
     public static String RSA_PRIVATE_KEY = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCRMwTrh5BHBgicgFLsuKakLU9roz+tEv+rFTyvkaeuRKRDYzRQ/NBpS8SuQDsn66wLYctfNz3Jza+rpFl95G8jAW/maXR00dbCDIwgAc1i8JjzOm/OlU2JzonXGF//39XNrKt1JMxbcoG9TbBGVlBc9IqF9NKlwzvDOMrp/AH4KDryGXcl/KeiB9DBgFdJJ9y2dsIcQOLZ9ev11DhKbhRNMn7i9YiptqRynX8x9HMymGuCQYXZjoZWWTi1eaSY8e7Hh37+uT0IfoYNHgfc8ppJbAQaRWvn3Pcsmfyl2aFX2gj4o1EO4ghXyzUGsz2yLcOCMG/168E+yor8044brxslAgMBAAECggEBAIbzQzxhJ8BLJE/ImMn5KLBoiGtDNBh5EvUAbQHEv3MhywXyuxoWjuF+kVNe0VxeFu4RO4LIhf381KwTuTVfzcdcO5OMKbe1nAOIw+evpSOy+IT/qBD6BTHyq3wl5Hudn0pklIrw45qOx3SZMx98c36hremozOtRlEbyju1/0pi/7MWHIKof+pMICK11JKqKKImW4Kfa9SIxmJ0HUHM1hJLfy6hPEph0ux7AZ/P7cVa6+6h10zO4aGRAYj8ZTPCCZVU9vytObQwFC7faQtyZeyVizhTETcMpkzBfNB/6alVF+Zfw6Uxx67spxaJKtO/f68AYemuSHM56bRxM6QaAOeECgYEAx79WKVfMOincatkqSCzRzlAbVUTKAverruuIBO0wDn/8Qe6uwer/peYnauySiH4UMj0pde4TJlsQLgejWV/3lKIVevKLQUPQNvjAQdGQ+92A4gQoZC+CJjty+w2DwbFV8JdPGXXNfRWXsAr7x6f198Qw9FX2WqxhKK6BqDFR6ZkCgYEAuhcSG0tioLTTpd5kmbJofmx6m/tjRK4q1vinhb39+e5cx72KJgNWb4CmJgFkKfuKBrY/5OBKl4lM+TbTqJKsf8e3mtzPm16pEsCR5Pcmy/j+8YUfKsylC6XOYOo+GdpV/H2LeJkJjG9fFCURLJrvdk53RT1dpp70yTWx/8ps7W0CgYEAvCvxFHOEjoqe/f1lSqcGY3Ctvy9Vi2Qc1WVHNzTm39IMJ2SeN0qV2YivO5hwFquHhSh/JXzJh4mj/WjupqS0jM4ApFlXfH1Q6mh+XcL57XvnDYTTNEZZOoEouX8qz06rYkJ4nsiOc7bitlIyjQ0at3WYlKVitlrMENIMjREhVIkCgYBla4l8wrtAJw1qyyg89BUrQegY9wuFNP84JlD2/TNcceKzIcQXL55+Krli4UM79lGlktKD5RICQNxovs7ggv/srGaOymWDojW3eKiLB4Bnjo80Bn+UDfCtxnISM9ggsvRGJg5LbezcUx6YlhMkIlT8P4uVevp9gwiaWN9oL2TqiQKBgAiaYeJc3gy4oU6aH1d8MKLzNOJKZDVOyvbZ6cYu21gJvgX4VBM/2yRweqVqcmVP1DPtfRpupkH0d5YvM6QYTR+MTYggthwHmkxpLASJRGiJ92d22XPzA/VJBDWWR7MrEJK+Poj6H88N7cACNvkFAHs/Ym+373e4pUZzrbWwLFtI";
     // 服务器异步通知页面路径 需http://或者https://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-    public static String notify_url = "http://www.idse.top";
+    public static String notify_url = "http://www.huijia21.com";
     // 页面跳转同步通知页面路径 需http://或者https://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问 商户可以自定义同步跳转地址
-    public static String return_url = "http://www.idse.top";
+    public static String return_url = "http://www.huijia21.com";
     // 请求网关地址,沙箱是:https://openapi.alipaydev.com/gateway.do
     public static String URL = "https://openapi.alipaydev.com/gateway.do";
     // 编码
